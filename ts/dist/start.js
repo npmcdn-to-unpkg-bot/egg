6fab0d3f678bf6d0fd73c7fd4c9f403379f0c081
braQweny <brakweny@gmail.com>
2016-07-01 17:52:35 +0200

    adD

---
+++ ts/dist/start.js
@@ -0,0 +1,11 @@
+"use strict";
+var APP = (function () {
+    function APP() {
+        this.title = "dasda";
+    }
+    APP.prototype.getUsers = function () {
+        return [{ name: "Franek" }];
+    };
+    ;
+    return APP;
+}());
---
+++ ts/src/start.ts
@@ -0,0 +1,12 @@
+import {SocialNetwork}from './interface.ts'
+
+class APP implements SocialNetwork{
+
+    title = "dasda";
+
+    getUsers(){
+
+        return [{name:"Franek"}]
+    };
+
+}
